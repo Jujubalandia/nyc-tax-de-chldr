2061a4d427a485b569d76b7369171723ba29735c
Bruno Moura <beralabs@gmail.com>
2020-02-26 18:00:19 +0000

    naive approach to upload files to s3 using boto3

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import os
+import sys
 import requests
 import time
-from os import environ
-
-
+import logging
+import boto3
+from botocore.exceptions import ClientError
 from os import listdir
 from os.path import isfile, join
-
+from os import environ
 
 def fetch_jsons(entry):
     path, uri = entry
@@ -21,10 +22,23 @@ def fetch_jsons(entry):
     print(time.time() - start)
     return path
 
+def upload_file(file_name, bucket, object_name=None):
+    
+    if object_name is None:
+        object_name = file_name
+
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
 
 if __name__ == "__main__":
 
     base_url = "https://s3.amazonaws.com/data-sprints-eng-test/"
+    bucket_name = 'de-bera-nyc-cab-trips'
     payloads = [
         ("data-files/data-nyctaxi-trips-2009.json", base_url + "data-sample_data-nyctaxi-trips-2009-json_corrigido.json"),
         ("data-files/data-nyctaxi-trips-2010.json", base_url + "data-sample_data-nyctaxi-trips-2010-json_corrigido.json"),
@@ -36,6 +50,5 @@ if __name__ == "__main__":
 
     for entry in payloads:
         fetch_jsons(entry)
-
-    
+        upload_file(entry[0],bucket_name)     
 
